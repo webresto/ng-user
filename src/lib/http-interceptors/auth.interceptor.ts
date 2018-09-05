d1eb952bd6715d608be7eecca61d331d69938c98
Дмитрий <d.koval7@gmail.com>
2018-09-05 15:13:02 +0300

    jwt fix

--- src/lib/http-interceptors/auth.interceptor.ts
+++ src/lib/http-interceptors/auth.interceptor.ts
@@ -23,7 +23,7 @@ export class AuthInterceptor implements HttpInterceptor {
       // Clone the request and replace the original headers with
       // cloned headers, updated with the authorization.
       const authReq = req.clone({
-        headers: req.headers.set('Authorization', authToken)
+        headers: req.headers.set('Authorization', `JWT ${authToken}`)
       });
 
       // send cloned request with header to the next handler.
