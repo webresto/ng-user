e78602554a0169f81998d9abde41b3227992251f
Gapp0 <jday.yodo@gmail.com>
2019-07-11 23:17:24 +0300

    1) chec в корзине тотправляется не на каждую введенную букву, а через определенное количество милисекунд после ввода последней 2) разкометирована кнопка "Войт/Зарегистрироваться" 3) в Readme добавлена информация о build

--- dist/lib/http-interceptors/index.d.ts
+++ dist/lib/http-interceptors/index.d.ts
@@ -1,6 +1,7 @@
 import { AuthInterceptor } from './auth.interceptor';
 export declare const ngUserHttpInterceptorProviders: {
-    provide: import("../../../../../../../../../../Users/user/projects/gfcafe/node_modules/@angular/core/src/di/injection_token").InjectionToken<import("../../../../../../../../../../Users/user/projects/gfcafe/node_modules/@angular/common/http/src/interceptor").HttpInterceptor[]>;
+    provide: import('@angular/core/src/di/injection_token')
+      .InjectionToken<import('@angular/common/http/src/interceptor').HttpInterceptor[]>;
     useClass: typeof AuthInterceptor;
     multi: boolean;
 }[];
