c1b9d78405178d75d68165bcfa8dc3ced374d9d3
Siziy Ivan <cziy@ya.ru>
2020-08-18 17:31:06 +0300

    update profile-response-data

---
+++ dist/esm5/lib/interfaces/profile-response-data.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/**
+ * @record
+ */
+export function ProfileResponseData() { }
+/** @type {?} */
+ProfileResponseData.prototype.token;
+/** @type {?} */
+ProfileResponseData.prototype.user;
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJvZmlsZS1yZXNwb25zZS1kYXRhLmpzIiwic291cmNlUm9vdCI6Im5nOi8vQHdlYnJlc3RvL25nLXVzZXIvIiwic291cmNlcyI6WyJsaWIvaW50ZXJmYWNlcy9wcm9maWxlLXJlc3BvbnNlLWRhdGEudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFVzZXIgfSBmcm9tIFwiLi91c2VyXCI7XG5cbmV4cG9ydCBpbnRlcmZhY2UgUHJvZmlsZVJlc3BvbnNlRGF0YSB7XG4gIHRva2VuOiBzdHJpbmcsXG4gIHVzZXI6IFVzZXJcbn0iXX0=
---
+++ dist/lib/interfaces/profile-response-data.d.ts
@@ -0,0 +1,5 @@
+import { User } from "./user";
+export interface ProfileResponseData {
+    token: string;
+    user: User;
+}
